440b1c1a35d4036bbb9723a290325ebd2d0efa48
jonathansolomon <jonathansolomon@192.168.0.23>
2018-08-01 19:54:08 -0400

    Initial checkin of database artifacts for tips_api

---
+++ resources/db/changelog/2018-08/2018-08-01-create-table-tips_info.sql
@@ -0,0 +1,14 @@
+--liquibase formatted sql
+--changeset Jonathan.Solomon:2018-08-01-jonathan-01
+CREATE TABLE IF NOT EXISTS tips-info
+(
+    tips_id       	UUID       		DEFAULT gen_random_uuid(),
+    first_name		VARCHAR(50),
+    last_name		VARCHAR(50),
+    email			VARCHAR(30),
+    phone			VARCHAR(12),
+    description		VARCHAR(1000)	NOT NULL,
+    created_date 	TIMESTAMP 		NOT NULL DEFAULT NOW(),
+    updated_date 	TIMESTAMP 		NOT NULL DEFAULT NOW(),
+    CONSTRAINT tips_pk PRIMARY KEY (tips_id)
+);
