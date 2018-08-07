e2731275930ae2bec6d2b59c73b2f1d964df31fb
ukafrik <ukafrik@hotmail.com>
2018-08-07 19:49:19 -0400

    Updated entities after discovering class details were switched.

--- src/main/java/com/navitas/rfad/model/entity/Tips.java
+++ src/main/java/com/navitas/rfad/model/entity/Tips.java
@@ -1,5 +1,7 @@
 package com.navitas.rfad.model.entity;
 
+import java.sql.Timestamp;
+import java.util.Objects;
 import java.util.UUID;
 
 import javax.persistence.Column;
@@ -12,34 +14,111 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tips_info")
 public class Tips {
+	
+	
+	  private static final long serialVersionUID = 7659513807301586843L;
 
-	private static final long serialVersionUID = 7659513807301586843L;
+	  @Id
+	  @GeneratedValue(strategy=GenerationType.AUTO)
+	  @Column(name="tips_id", nullable=false)
+	  private UUID id;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "tips_id", nullable = false)
-	private UUID id;
+	  @Column(name="email", nullable=true)
+	 // @Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$", message = "Must be a valid email: 'user@test.com'")
+	  private String email;
 
-	@Column(name = "entity", nullable = true)
-	private String entity;
+	  @Column(name="first_name", nullable=true)
+	  private String firstName;
+	    
+	  @Column(name="last_name", nullable=true)
+	  private String lastName;
+	  
+	  @Column(name="phone", nullable=true)
+	  private String phone;
+	  
+	  @Column(name="description", nullable=false)
+	  private String description;
+	  
+	  @Column(name="created_date", nullable=true)
+	  private Timestamp createdDate;
+	    
+	  @Column(name="updated_date", nullable=true)
+	  private Timestamp updatedDate;
+	  
+	  public UUID getId() {
+	    return id;
+	  }
 
-	@Column(name = "status", nullable = true)
-	private String status;
+	  public void setId(UUID id) {
+	    this.id = id;
+	  }
 
-	public String getEntity() {
-		return entity;
-	}
+	  public String getEmail() {
+	    return email;
+	  }
 
-	public void setEntity(String entity) {
-		this.entity = entity;
-	}
+	  public void setEmail(String email) {
+	    this.email = email;
+	  }
+	  public String getFirstName() {
+	    return firstName;
+	  }
 
-	public String getStatus() {
-		return status;
-	}
+	  public void setFirstName(String firstName) {
+	    this.firstName = firstName;
+	  }
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
+	  public String getLastName() {
+	    return lastName;
+	  }
 
+	  public void setLastName(String lastName) {
+	    this.lastName = lastName;
+	  }
+	  
+	  
+	  public String getDescription() {
+	    return description;
+	  }
+
+	  public void setDescription(String description) {
+	    this.description = description;
+	  }
+
+	  public Timestamp getCreatedDate() {
+	   return createdDate;
+	  }
+
+	  public void setCreatedDate(Timestamp createdDate) {
+		this.createdDate = createdDate;
+	  }
+
+	  public Timestamp getUpdatedDate() {
+		return updatedDate;
+	  }
+
+	  public void setUpdatedDate(Timestamp updatedDate) {
+		this.updatedDate = updatedDate;
+	  }
+	  
+
+	  @Override
+	  public boolean equals(Object obj) {
+	    if (this == obj) {
+	      return true;
+	    }
+
+	    if (obj instanceof Tips) {
+	      final Tips other = (Tips) obj;
+	      return Objects.equals(id, other.getId()) || Objects.equals(description, other.getDescription());
+	    } else {
+	      return false;
+	    }
+	  }
+
+	  @Override
+	  public int hashCode() {
+	    return Objects.hash(id, description);
+	  }
+		
 }
--- src/main/java/com/navitas/rfad/model/entity/TipsFraudReport.java
+++ src/main/java/com/navitas/rfad/model/entity/TipsFraudReport.java
@@ -1,119 +1,48 @@
 package com.navitas.rfad.model.entity;
 
-import java.sql.Timestamp;
 import java.util.UUID;
-import java.util.Objects;
 
-import javax.persistence.*;
 //import javax.validation.constraints.Pattern;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="tips_info")
 public class TipsFraudReport {
-	
-  private static final long serialVersionUID = 7659513807301586843L;
 
-  @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
-  @Column(name="tips_id", nullable=false)
-  private UUID id;
 
-  @Column(name="email", nullable=true)
- // @Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$", message = "Must be a valid email: 'user@test.com'")
-  private String email;
+	private static final long serialVersionUID = 7659513807301586843L;
 
-  @Column(name="first_name", nullable=true)
-  private String firstName;
-    
-  @Column(name="last_name", nullable=true)
-  private String lastName;
-  
-  @Column(name="phone", nullable=true)
-  private String phone;
-  
-  @Column(name="description", nullable=false)
-  private String description;
-  
-  @Column(name="created_date", nullable=true)
-  private Timestamp createdDate;
-    
-  @Column(name="updated_date", nullable=true)
-  private Timestamp updatedDate;
-  
-  public UUID getId() {
-    return id;
-  }
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "tips_id", nullable = false)
+	private UUID id;
 
-  public void setId(UUID id) {
-    this.id = id;
-  }
+	@Column(name = "entity", nullable = true)
+	private String entity;
 
-  public String getEmail() {
-    return email;
-  }
+	@Column(name = "status", nullable = true)
+	private String status;
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
-  public String getFirstName() {
-    return firstName;
-  }
+	public String getEntity() {
+		return entity;
+	}
 
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
+	public void setEntity(String entity) {
+		this.entity = entity;
+	}
 
-  public String getLastName() {
-    return lastName;
-  }
+	public String getStatus() {
+		return status;
+	}
 
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-  
-  
-  public String getDescription() {
-    return description;
-  }
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
 
-  public Timestamp getCreatedDate() {
-   return createdDate;
-  }
-
-  public void setCreatedDate(Timestamp createdDate) {
-	this.createdDate = createdDate;
-  }
-
-  public Timestamp getUpdatedDate() {
-	return updatedDate;
-  }
-
-  public void setUpdatedDate(Timestamp updatedDate) {
-	this.updatedDate = updatedDate;
-  }
-  
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-
-    if (obj instanceof TipsFraudReport) {
-      final TipsFraudReport other = (TipsFraudReport) obj;
-      return Objects.equals(id, other.getId()) || Objects.equals(description, other.getDescription());
-    } else {
-      return false;
-    }
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, description);
-  }
-	
 }
